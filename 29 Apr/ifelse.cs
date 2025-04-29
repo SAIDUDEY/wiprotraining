748bd71f47195efe9ef214d78ce543090c7833c9
SAIDUDEY <dudesai9@gmail.com>
2025-04-29 16:12:12 +0530

    code

---
+++ 29 Apr/ifelse.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Ifesle
+{
+    public static void Main(string[] args)
+    {
+        int num = 11;
+        if(num%2==0)
+    {
+        Console.WriteLine("even");
+    }
+    else
+    Console.WriteLine("odd");
+    }
+}
