dea0bbf747153c3153d5b24085323aeeb573e96c
SAIDUDEY <dudesai9@gmail.com>
2025-04-29 15:37:39 +0530

    code

---
+++ 29 Apr/div.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Addition{
+    
+    static int add(int a,int b){
+        return a / b;
+        }
+    static void Main(){
+        int numb1 = 22;
+        int numb2 = 33;
+        Console.WriteLine("Addition: " + add(numb1, numb2));
+    }
+}
---
+++ 29 Apr/var.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int age : 22;
+        string name : jhonny;
+        double salary : 25000.00;
+        bool isemployee : true;
+
+        Console.WriteLine ("age :" + age);
+        Console.WriteLine ("string :" + string);
+        Console.WriteLine ("salary :" + salary);
+        Console.WriteLine ("hello world");
+    }
+}
