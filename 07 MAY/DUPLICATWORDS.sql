f6ec1ab772c7172726cc8e1c685f555a3574a727
SAIDUDEY <dudesai9@gmail.com>
2025-05-07 10:15:26 +0530

    C#

---
+++ 07 MAY/DUPLICATWORDS.sql
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics.Tracing;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+
+        string[] s = str.Split(" ");
+
+        bool[] b = new bool[s.Length];
+
+        Method(s, b);
+
+    }
+    public static void Method(string[] s, bool[] b)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            if (b[i] == false)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    
+                        if (s[i] == s[j])
+                        {
+                            b[j] = true;
+                        }
+                    }
+                }
+            }
+            for (int k = 0; k < s.Length; k++)
+            {
+                {
+                    if (b[k] == true)
+                    {
+                        Console.WriteLine(s[k]);
+                    }
+                }
+
+
+
+
+
+
+            }
+        }
+    }
+}
