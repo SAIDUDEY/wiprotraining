ce9f39893512700557275487a4ce6925e6fcb5b1
SAIDUDEY <dudesai9@gmail.com>
2025-05-07 14:43:54 +0530

    c#

---
+++ 07 MAY/BASIC-ARRAYLIST-OPERATIONS.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList names = new ArrayList() {"sai", "bala", "dude", "vinay" };
+        names.Sort();
+
+        Console.WriteLine("enter a student name :");
+        string NametoCheck = Console.ReadLine();
+
+        if (names.Contains(NametoCheck))
+        {
+            Console.WriteLine("name found: " + NametoCheck);
+        
+        }
+        else
+        {
+            Console.WriteLine("not exists: " + NametoCheck);
+        }
+
+        Console.WriteLine("final list : ");
+            foreach (var num in names)
+            
+        {
+                Console.WriteLine(num);
+            }
+        
+
+    }
+}
