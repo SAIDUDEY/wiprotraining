64fc62bcf32e446196dbb0f6c4b6195a812a04c7
SAIDUDEY <dudesai9@gmail.com>
2025-05-08 10:23:33 +0530

    c#

--- 08 may/liststudents.cs
+++ 08 may/liststudents.cs
@@ -1,6 +1,7 @@
 using System;
 
-class Student {
+class Student
+{
     public int ID { get; set; }
     public string Name { get; set; }
 }
@@ -9,15 +10,18 @@ class Program
 {
     static void Main()
     {
-        List<Student> students = new List<Student> 
+        List<Student> students = new List<Student>
         {
-        new Student (ID = 1; Name = "alice" );
-        new Student (ID = 2; name = "bob");
+           new Student {ID = 1, Name = "alice" },
+           new Student{ID = 2, Name = "bob" }
 
-    };
+        };
 
-    students.Add(new Student {ID = 3; Name = "loe" });
-foreach (var Student in students) {
+    students.Add(new Student {ID = 3, Name = "loe" });
+
+
+foreach (var student in students)
+{
     Console.WriteLine($"ID:{student.ID}, Name: {student.Name}");
 }
 
