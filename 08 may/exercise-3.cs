068b8aa3c27b6dcc1e563612e31b28b931996097
SAI DUDEY <124022270+SAIDUDEY@users.noreply.github.com>
2025-05-08 17:31:34 +0530

    c#

--- 08 may/exercise-3.cs
+++ 08 may/exercise-3.cs
@@ -1,44 +1,44 @@
 using System;
-using System.Diagnostics;
+using System.Collections;
 
-class Student
-{
-    public int ID { get; set; }
-    public string Name { get; set; }
-
-    public double Price { get; set; }
-}
-
-class Program
-{
-    static void Main()
+class Product
     {
-        List<Product> products = new List<Product>();
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price{ get; set; }
+        
+    }
+    class Program
+    {
+        static void Main()
         {
-          products.Add(  new Product { ID = 1, Name = "laptop", Price = 50000.00 });
-          products.Add(  new Product { ID = 2, Name = "TV", Price = 43000.00 });
-          products.Add(  new.Product {ID = 3, Name = "mobile", Price = 80000.00});
-          products.Add( new.Product {ID = 3, Name = "AC", Price = 40000.00});
-          products.Add( new.Product {ID = 3, Name = "fridge", Price = 20000.00});
-        };
 
-        var decendingorder = products.OrderByDescending(p => p.Price);
+            List<Product> products = new List<Product>();
 
-        Console.WriteLine("desencingorder : ");
+            products.Add(new Product { Id = 1, Name = "Laptop", Price = 50000.0 });
+            products.Add(new Product { Id = 2, Name = "TV", Price = 40000.0 });
+            products.Add(new Product { Id = 3, Name = "AC", Price = 80000.0 });
+            products.Add(new Product { Id = 4, Name = "Headset", Price = 6000.0 });
+            products.Add(new Product { Id = 5, Name = "mobile", Price = 40000.0 });
 
-        foreach (var item in decendingorder)
-        {
-            Console.WriteLine(item.price);
+            //Descending order by their name
+            var descProducts =  products.OrderByDescending(e => e.Price);
 
+            foreach (var descProduct in descProducts)
+            {
+                Console.WriteLine($"{descProduct.Id}, {descProduct.Name}, {descProduct.Price}");
+            }
+
+            Console.WriteLine();
+
+            //Highest price and lowest
+            double  maxPrice = products.Max(e => e.Price);
+
+            Console.WriteLine("The highest Price:" + maxPrice);
+
+            double minPrice = products.Min(e => e.Price);
+
+            Console.WriteLine("The Lowest Price:" + minPrice);
 
         }
-        var max = products.Max(p => p.Price);
-        Console.WriteLine("highest price :" + max);
-        
-        var min = products.Min(p => p.Price);
-        Console.WriteLine("lowest price :" + min);
-
-
-
     }
-}
