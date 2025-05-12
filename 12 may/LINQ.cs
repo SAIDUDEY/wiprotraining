63f9ccb9cecda6e63db1e990830e9e980ddb93ea
SAIDUDEY <dudesai9@gmail.com>
2025-05-12 16:09:07 +0530

    c#

---
+++ 12 may/LINQ.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "anil", "sharma", "abdhullah", "imran", "siva", "naresh" , "suresh", "suman" };
+
+        UsingLinq(names);
+    }
+
+    private static void UsingLinq(string[] names)
+    {
+        IEnumerable<string> query = from s in names
+                                    where s.Length == 5
+                                    orderby s
+                                    select s.ToUpper();
+
+        foreach (string item in query)
+        {
+            Console.WriteLine(item);
+
+            Console.Read();
+        }
+    }
+}
