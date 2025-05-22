5b8e388ecc37e31d26f610c8c045b97ad7786a98
SAIDUDEY <dudesai9@gmail.com>
2025-05-22 10:09:44 +0530

    c#

---
+++ 22 may/exercise 1/ex3.cs
@@ -0,0 +1,29 @@
+class Product
+{
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+}
+
+class Program
+{ 
+static void Main()
+{
+    List<Product> products = new List<Product>();
+
+    products.Add(new Product { Name = "plates", Price = 500 });
+    products.Add(new Product { Name = "glass", Price = 700 });
+    products.Add(new Product { Name = "tv", Price = 30000 });
+        products.Add(new Product { Name = "phone", Price = 3000 });
+        products.Add(new Product { Name = "buds", Price = 1300 });
+
+        var linqProduct = products.Where(p => p.Price > 500);
+
+        foreach (var product in linqProduct)
+        {
+            Console.WriteLine("product names : " + product.Name + " ");
+        }
+
+
+    }
+}
